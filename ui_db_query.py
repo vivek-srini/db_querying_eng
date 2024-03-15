4e0983d2cc3b54c5d443aaeaed7e9416fe0f3343
vivek-srini <38190456+vivek-srini@users.noreply.github.com>
2024-03-15 18:18:55 +0530

    Update ui_db_query.py

--- ui_db_query.py
+++ ui_db_query.py
@@ -257,7 +257,7 @@ Please understand that you are not chatting with me. Rather, you simply have to 
 Note: IT IS OF UTMOST IMPORTANCE THAT YOU DO NOT MENTION THE JSON AT ALL. ALSO YOU ARE SUPPOSED TO GIVE A VERBAL ANSWER TO THE USER AND NOT WRITE ANY CODE OR GIVE ANY OTHER INSTRUCTION. DIRECTLY ANSWER THE USER'S QUESTION"""
   #stage3_response = get_chat_response_closed(stage3_prompt,"gpt-3.5-turbo-0125")
   stage3_response = answer_with_haiku(stage3_prompt)
-  total_cost = (num_tokens_from_string(stage2_prompt)*30 + client.count_tokens(stage1_prompt+stage3_prompt)*0.25+num_tokens_from_string(stage2_response)*1.5+client.count_tokens(sql_query+stage3_response)*1.25)/1000000
+  total_cost = (client.count_tokens(stage2_prompt)*0.25 + client.count_tokens(stage1_prompt+stage3_prompt)*0.25+client.count_tokens(stage2_response)*1.25+client.count_tokens(sql_query+stage3_response)*1.25)/1000000
   st.write("Total Cost:",f"{total_cost}$")
   
 
